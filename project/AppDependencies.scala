9e2260654071ee9ab132fcbc0fda68cacbf27df9
George Westwater <gmwestwater@mtdapi>
2018-05-24 15:25:18 +0100

    Updated dependencies (#1)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,15 +7,16 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.5.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.5.0",
+    "uk.gov.hmrc" %% "auth-client" % "2.6.0"
   )
 
   def test(scope: String = "test") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
-    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.5" % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-
+    "org.mockito" % "mockito-core" % "2.18.3" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
   )
-
 }
