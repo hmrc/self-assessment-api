eb8caae2e5c562a6c8cb721089bacd1f35b14703
Daniel Andrews <daniel.andrews@digital.hmrc.gov.uk>
2018-11-26 13:39:20 +0000

    Bob

---
+++ test/uk/gov/hmrc/r2/selfassessmentapi/models/des/FinancialsSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.r2.selfassessmentapi.models.des
+
+import uk.gov.hmrc.selfassessmentapi.models
+import uk.gov.hmrc.selfassessmentapi.models.des.selfemployment.{Deduction, Financials}
+import uk.gov.hmrc.selfassessmentapi.models.{Expense, ExpenseNegativeOrPositive, SimpleIncome, ExpenseProfessionalFees}
+import uk.gov.hmrc.selfassessmentapi.resources.JsonSpec
+
+class FinancialsSpec extends JsonSpec {
+  "from" should {
+    "correctly map a API self-employment update into a DES self-employment update" in {
+      val apiUpdate = models.selfemployment.SelfEmploymentPeriodUpdate(
+        incomes =
+          Some(models.selfemployment.Incomes(turnover = Some(SimpleIncome(10.10)), other = Some(SimpleIncome(10.10)))),
+        expenses = Some(
+          models.selfemployment.Expenses(
+            cisPaymentsToSubcontractors = Some(Expense(10.10, Some(10.10))),
+            depreciation = Some(ExpenseNegativeOrPositive(10.10, Some(10.10))),
+            costOfGoodsBought = Some(ExpenseNegativeOrPositive(10.10, Some(10.10))),
+            professionalFees = Some(ExpenseProfessionalFees(10.10, Some(10.10))),
+            badDebt = Some(Expense(10.10, Some(10.10))),
+            adminCosts = Some(Expense(10.10, Some(10.10))),
+            advertisingCosts = Some(Expense(10.10, Some(10.10))),
+            businessEntertainmentCosts = Some(Expense(10.10, Some(10.10))),
+            financialCharges = Some(ExpenseNegativeOrPositive(10.10, Some(10.10))),
+            interest = Some(ExpenseNegativeOrPositive(10.10, Some(10.10))),
+            maintenanceCosts = Some(ExpenseNegativeOrPositive(10.10, Some(10.10))),
+            premisesRunningCosts = Some(ExpenseNegativeOrPositive(10.10, Some(10.10))),
+            staffCosts = Some(Expense(10.10, Some(10.10))),
+            travelCosts = Some(Expense(10.10, Some(10.10))),
+            other = Some(Expense(10.10, Some(10.10)))
+          )),
+        consolidatedExpenses = None)
+
+      val desUpdate = Financials.from(apiUpdate)
+      val desIncomes = desUpdate.incomes.get
+      val desDeductions = desUpdate.deductions.get
+
+      desIncomes.turnover shouldBe Some(10.10)
+      desIncomes.other shouldBe Some(10.10)
+
+      desDeductions.constructionIndustryScheme shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.depreciation shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.costOfGoods shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.professionalFees shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.badDebt shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.adminCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.advertisingCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.businessEntertainmentCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.financialCharges shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.interest shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.maintenanceCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.premisesRunningCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.staffCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.travelCosts shouldBe Some(Deduction(10.10, Some(10.10)))
+      desDeductions.other shouldBe Some(Deduction(10.10, Some(10.10)))
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/r2/selfassessmentapi/resources/SelfEmploymentBISSResourceSpec.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.r2.selfassessmentapi.resources
+
+import org.omg.CosNaming.NamingContextPackage.NotFound
+import play.api.libs.json.Json.toJson
+import play.api.test.FakeRequest
+import uk.gov.hmrc.selfassessmentapi.fixtures.selfemployment.SelfEmploymentBISSFixture
+import uk.gov.hmrc.selfassessmentapi.mocks.services.MockSelfEmploymentBISSService
+import uk.gov.hmrc.selfassessmentapi.models.Errors._
+import uk.gov.hmrc.selfassessmentapi.models.SourceType
+
+import scala.concurrent.Future
+
+class SelfEmploymentBISSResourceSpec extends ResourceSpec
+  with MockSelfEmploymentBISSService {
+
+  class Setup {
+    val resource = new SelfEmploymentBISSResource {
+      override val appContext = mockAppContext
+      override val authService = mockAuthorisationService
+      override val service = mockSelfEmploymentBISSService
+    }
+    mockAPIAction(SourceType.SelfEmployments)
+  }
+
+  val selfEmploymentBISS = SelfEmploymentBISSFixture.selfEmploymentBISS
+  val selfEmploymentBISSJson = SelfEmploymentBISSFixture.selfEmploymentBISSJson
+  val selfEmploymentId = "test-source-id"
+
+  "getSummary" should {
+    "return a 200 with a SelfEmploymentBISS response" when {
+      "a valid nino and tax year is supplied and DES return a SelfEmploymentBISS response" in new Setup {
+        MockSelfEmploymentBISSService.getSummary(nino, taxYear, selfEmploymentId)
+          .returns(Future.successful(Right(selfEmploymentBISS)))
+
+        val result = resource.getSummary(nino, taxYear, selfEmploymentId)(FakeRequest())
+        status(result) shouldBe OK
+        contentType(result) shouldBe Some(JSON)
+        contentAsJson(result) shouldBe selfEmploymentBISSJson
+      }
+    }
+
+    Seq(
+      BAD_REQUEST -> NinoInvalid,
+      BAD_REQUEST -> TaxYearInvalid,
+      BAD_REQUEST -> SelfEmploymentIDInvalid,
+      NOT_FOUND -> NoSubmissionDataExists,
+      INTERNAL_SERVER_ERROR -> ServerError,
+      INTERNAL_SERVER_ERROR -> ServiceUnavailable
+    ).foreach { case (responseCode, errorCode) =>
+
+      s"return a status ($responseCode)" when {
+        s"a ${errorCode.code} error is returned from the connector" in new Setup {
+          MockSelfEmploymentBISSService.getSummary(nino,taxYear, selfEmploymentId)
+            .returns(Future.successful(Left(ErrorWrapper(errorCode, None))))
+
+          val result = resource.getSummary(nino, taxYear, selfEmploymentId)(FakeRequest())
+          status(result) shouldBe responseCode
+          contentType(result) shouldBe Some(JSON)
+          contentAsJson(result) shouldBe toJson(errorCode)
+        }
+      }
+    }
+  }
+}
