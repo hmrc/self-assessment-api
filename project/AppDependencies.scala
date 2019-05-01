4dcb54197de7269fc5c0b9bcd5c0be02e710385a
Daniel Andrews <38686400+daniel-andrews1994@users.noreply.github.com>
2019-05-01 09:49:10 +0100

    Updated dependencies (#211)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcTestVersion = "3.7.0-play-25"
+  private val hmrcTestVersion = "3.8.0-play-25"
   private val scalaTestVersion = "3.0.5"
   private val scalaTestPlusVersion = "2.0.1"
   private val pegdownVersion = "1.6.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.10.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.11.0",
     "uk.gov.hmrc" %% "auth-client" % "2.20.0-play-25",
     "uk.gov.hmrc" %% "play-hmrc-api" % "3.2.0"
   )
