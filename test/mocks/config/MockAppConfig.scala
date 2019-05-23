ff95905904f7d2c7da0c7120ceb4e332cdcc1b81
MarkAKelly <21219364+MarkAKelly@users.noreply.github.com>
2019-05-23 13:04:41 +0100

    [MTDSA-3534] Remove references to ServiceLocator

--- test/mocks/config/MockAppConfig.scala
+++ test/mocks/config/MockAppConfig.scala
@@ -28,13 +28,11 @@ trait MockAppConfig extends Mock { _: Suite =>
 
   object MockAppConfig {
     def appName: OngoingStubbing[String] = when(mockAppConfig.appName)
-    def appNameForServiceLocator: OngoingStubbing[String] = when(mockAppConfig.appNameForServiceLocator)
     def appUrl: OngoingStubbing[String] = when(mockAppConfig.appUrl)
     def apiStatus(version: String): OngoingStubbing[String] = when(mockAppConfig.apiStatus(any[String]()))
     def featureSwitch: OngoingStubbing[Option[Configuration]] = when(mockAppConfig.featureSwitch)
     def registrationEnabled: OngoingStubbing[Boolean] = when(mockAppConfig.registrationEnabled)
 
-    def serviceLocatorUrl: OngoingStubbing[String] = when(mockAppConfig.serviceLocatorUrl)
     def saApiUrl: OngoingStubbing[String] = when(mockAppConfig.saApiUrl)
     def cgApiUrl: OngoingStubbing[String] = when(mockAppConfig.cgApiUrl)
     def taxCalcUrl: OngoingStubbing[String] = when(mockAppConfig.taxCalcUrl)
