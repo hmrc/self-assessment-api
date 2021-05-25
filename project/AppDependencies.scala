146a3965f51ccd3af547186ea8965a05951e55d9
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2021-05-25 14:07:52 +0100

    Revert "MTDSA-8814 Dependency Upgrades up before play 28 with artifactory still about"
    This reverts commit 69da2e08fabd6bbf7f8c5738951c52869a8e6969.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,18 +7,18 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-26" % "3.2.0",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.1" cross CrossVersion.full),
-    "com.github.ghik" % "silencer-lib" % "1.7.1" % Provided cross CrossVersion.full
+    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.6.0" cross CrossVersion.full),
+    "com.github.ghik" % "silencer-lib" % "1.6.0" % Provided cross CrossVersion.full
   )
 
   def test(scope: String = "test, it") = Seq(
-    "org.scalatest"          %% "scalatest"             % "3.2.7"             % scope,
-    "org.scalacheck"         %% "scalacheck"            % "1.15.3"            % scope,
+    "org.scalatest"          %% "scalatest"             % "3.2.3"             % scope,
+    "org.scalacheck"         %% "scalacheck"            % "1.15.1"            % scope,
     "com.vladsch.flexmark"   % "flexmark-all"           % "0.36.8"           % scope,
     "org.scalamock"          %% "scalamock"             % "5.1.0"             % scope,
     "org.scalatestplus"      %% "scalatestplus-mockito" % "1.0.0-M2"          % scope,
     "com.typesafe.play"      %% "play-test"             % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"    % "5.1.0"             % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play"    % "3.1.3"             % scope,
     "org.mockito"            %  "mockito-core"          % "3.6.28"             % scope,
     "com.github.tomakehurst" % "wiremock-jre8"          % "2.27.2"            % scope
   )
